9a6619221bc1d833211bb978a4aad92f248160af
isabella <89166884+yqw7@users.noreply.github.com>
2021-11-17 12:15:20 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-from flask import render_template, request
-from __init__ import app
+# import "packages" from flask
+from flask import Flask, render_template
 
-from starter.starter import app_starter
-from algorithm.algorithm import algorithm_bp
-from api.webapi import api_bp
-from crud.crud import model_bp
-from y2022 import y2022_bp
-
-app.register_blueprint(app_starter)
-app.register_blueprint(algorithm_bp)
-app.register_blueprint(api_bp)
-app.register_blueprint(model_bp)
-app.register_blueprint(y2022_bp)
+# create a Flask instance
+app = Flask(__name__)
 
 
+# connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
-    # starting and empty input default
-    return render_template("greet.html", name="World")
+# connects /kangaroos path to render kangaroos.html
+@app.route('/kangaroos/')
+def kangaroos():
+    return render_template("kangaroos.html")
 
 
-@app.route('/deploy')
-def deploy():
-    return render_template("course/deploy.html")
+@app.route('/walruses/')
+def walruses():
+    return render_template("walruses.html")
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+@app.route('/hawkers/')
+def hawkers():
+    return render_template("hawkers.html")
 
 
+@app.route('/stub/')
+def stub():
+    return render_template("stub.html")
+
+
+# runs the application on the development server
 if __name__ == "__main__":
-    # runs the application on the repl development server
-    app.run(debug=True, port="5222")
+    app.run(debug=True)
