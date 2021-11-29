a0d677a84309f7b841ab3882eb1284ecd71e1c10
calissa <calissat@gmail.com>
2021-11-29 13:04:09 -0800

    api and home page

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests, json
+
+
 # import "packages" from flask
 from flask import Flask, render_template
 
@@ -9,6 +12,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
 
 @app.route('/Ethan/')
 def stub2():
@@ -25,21 +30,38 @@ def kangaroos():
 def walruses():
     return render_template("walruses.html")
 
+
 @app.route('/abouthassan/')
 def abouthassan():
     return render_template("abouthassan.html")
+
 
 @app.route('/isabella/')
 def isabella():
     return render_template("isabella.html")
 
+
 @app.route('/calissa/')
 def calissa():
-    return render_template("calissa.html")
+    url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+
+    payload = "q=Hello%2C%20world!&target=es&source=en"
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'accept-encoding': "application/gzip",
+        'x-rapidapi-host': "google-translate1.p.rapidapi.com",
+        'x-rapidapi-key': "d7c8c8031emshc7982f51947d6d4p19192fjsnc28f123f5152"
+}
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    text = response.json()
+    return render_template("calissa.html", text=text)
+
 
 @app.route('/nicolas/')
 def nicolas():
     return render_template("nicolas.html")
+
 
 @app.route('/hawkers/')
 def hawkers():
@@ -49,7 +71,6 @@ def hawkers():
 @app.route('/stub/')
 def stub():
     return render_template("stub.html")
-
 
 
 # runs the application on the development server
